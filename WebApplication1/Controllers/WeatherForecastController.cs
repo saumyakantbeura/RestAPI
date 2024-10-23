31a71303d0ab976c29aa96b87b153a7e6a14d81d
Saumyakant Beura <E010409@cigniti.com>
2024-10-23 14:13:24 +0530

    fix build issue

--- WebApplication1/Controllers/WeatherForecastController.cs
+++ WebApplication1/Controllers/WeatherForecastController.cs
@@ -30,16 +30,5 @@ namespace WebApplication1.Controllers
             .ToArray();
         }
 
-        [HttpGet(Name = "GetWeatherForecastById")]
-        public IEnumerable<WeatherForecast> GetById(int ByIdDeatailsS)
-        {
-            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
-            {
-                Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-                TemperatureC = Random.Shared.Next(-20, 55),
-                Summary = Summaries[Random.Shared.Next(Summaries.Length)]
-            })
-            .ToArray();
-        }
     }
 }
